e71d5b764679889018001827774a65ad16825a69
lw-92 <670415962@qq.com>
2019-11-09 14:37:18 +0800

    结构初始化，加入了jar包

---
+++ springsecurityweixin/src/main/java/com/lee/tech/weixin/Application.java
@@ -0,0 +1,17 @@
+package com.lee.tech.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wei.li
+ * @date 2019/11/9 14:23
+ * @desc
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
